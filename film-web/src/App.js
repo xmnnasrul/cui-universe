11a0f93b5a752015d24fa8b32ab2d170708774c8
xmnscript <mygithub.nasrul@gmail.com>
2022-11-19 09:26:35 +0700

    add react movie

--- film-web/src/App.js
+++ film-web/src/App.js
@@ -1,29 +1,48 @@
-import './App.css';
-import {getMovieList, searchMovie} from "./api"
-const  App = ()=>{
-  const search = (q) => {
-    console.log({ q })
-  }
+import "./App.css";
+import { getMovieList, searchMovie } from "./api";
+import { useEffect, useState } from "react";
+
+const App = () => {
+  const [popularMovie, setPopularMovie] = useState({});
+
   useEffect(() => {
-    getMovieList()
-  },[])
-  
+    getMovieList().then((result) => {
+      setPopularMovie(result);
+    });
+  }, []);
+
+const popularMovieList = () => {
+  return popularMovie.map((movie, i) => {
+    return(
+        <div className="Movie-wrapper" key={i}>
+            <div className="Movie-title">{movie.title}</div>
+            <img className="Movie-img"  src={`${process.env.REACT_APP_BASEIMGURL}/movie.poster_path`} alt="img"/>
+            <div className="Movie-date">release: {movie.release_date}</div>
+            <div className="Movie-rating">{movie.vote_everage}</div>
+          </div>
+    )
+  })
+}
+
+  const search = async (q) => {
+    if (q.lengh>3) {
+      const query = await searchMovie(q)
+      setPopularMovie(query.results)
+    }
+
+  };
+
   return (
-    <div className="App" >
+    <div className="App">
       <header className="App-header">
         <h1>Movie</h1>
-        <input className="Movie-search" placeholder="search" onChange={({target})=> search(target.value )} />
+        <input className="Movie-search" placeholder="search" onChange={({ target }) => search(target.value)} />
         <div className="Movie-container">
-          <div className="Movie-wrapper">
-            <div className="Movie-title"></div>
-            <img className="Movie-img" />
-            <div className="Movie-date"></div>
-            <div className="Movie-rating"></div>
-          </div>
+          <popularMovieList />
         </div>
       </header>
     </div>
   );
-}
+};
 
 export default App;
--- film-web/src/api.js
+++ film-web/src/api.js
@@ -1,12 +1,17 @@
 import axios from "axios"
 
+const api = process.env.REACT_APP_APIKEY
+const baseurl = process.env.REACT_APP_BASEURL
+
 export const getMovieList = async() =>{
-const movie = await axios.get(`${process.env.REACT_APP_BASEURL}/movie/popular `)
-// return
-console.log({getMovieList:movie})
+const movie = await axios.get(`${baseurl}/movie/popular?page=1&api_key=${api}`)
+return movie.data.results
 }
+
+
 export const searchMovie = async(q)=>{
-    const search= await  axios.get(q)
-    return
+    const search= await  axios.get(`${baseurl}/search/movie?query=${q}&page=1&&api_key=${api}`)
+    
+    return search.data
 }
  
